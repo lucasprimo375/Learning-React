d774c7272f87b070b5efaf6d55a0eaeded9127a1
Lucas Primo <lucasprimo375@hotmail.com>
2023-02-10 09:34:43 -0300

    how to comment

--- 2_components/components/src/components/MyComponent.js
+++ 2_components/components/src/components/MyComponent.js
@@ -1,6 +1,16 @@
+// yet another comment
+
 const MyComponent = () => {
+    /**
+     * some comment
+     */
+
     return (
         <div>
+            {/* 
+                some comment
+            */}
+
             <h1 className="myClass">My Component</h1>
         </div>
     );
