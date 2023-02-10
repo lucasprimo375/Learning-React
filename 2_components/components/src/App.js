1b039a7b7308cd8bb17e05402baec3394199937d
Lucas Primo <lucasprimo375@hotmail.com>
2023-02-10 09:20:02 -0300

    Importing components

--- 2_components/components/src/App.js
+++ 2_components/components/src/App.js
@@ -1,9 +1,13 @@
+import MyComponent from './components/MyComponent';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React Components</h1>
+
+      <MyComponent></MyComponent>
     </div>
   );
 }
