9343d59afb8d0ad1515e63bd1b220a37f8da2f04
Lucas Primo <lucasprimo375@hotmail.com>
2023-02-14 09:32:07 -0300

    functions for rendering

--- 2_components/components/src/components/Events.js
+++ 2_components/components/src/components/Events.js
@@ -4,6 +4,14 @@ const Events = () => {
         
         console.log("Button was clicked!");
     };
+
+    const renderStuff = (shouldRender) => {
+        if (shouldRender) {
+            return <h1>Should render this</h1>
+        }
+
+        return <h1>Should render that</h1>
+    };
     
     return (
         <div>
@@ -19,6 +27,12 @@ const Events = () => {
                 <button onClick={() => {
                     console.log("You shouldn't do this")
                 }}>Yet another button! Click it!</button>
+            </div>
+
+            <div>
+                {renderStuff(true)}
+
+                {renderStuff(false)}
             </div>
         </div>
     );
