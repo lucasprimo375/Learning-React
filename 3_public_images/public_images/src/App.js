062b45dd3633ac406249dccaa8c7fe6df39d2803
Lucas Primo <lucasprimo375@hotmail.com>
2023-02-17 09:08:56 -0300

    accessing images in public folder

---
+++ 3_public_images/public_images/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avatar: The Way of Water</h1>
+
+      <div>
+        <img src="/avatar.jpg" alt="Avatar: The Way of Water poster" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
