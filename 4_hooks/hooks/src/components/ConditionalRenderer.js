50f8e313c02511dfa52a2ca68ba0c286be9b6506
Lucas Primo <lucasprimo375@hotmail.com>
2023-03-03 08:57:08 -0300

    more conditional rendering

--- 4_hooks/hooks/src/components/ConditionalRenderer.js
+++ 4_hooks/hooks/src/components/ConditionalRenderer.js
@@ -3,9 +3,23 @@ import { useState } from 'react'
 function ConditionalRenderer() {
     const condition = useState(true)
 
+    const [name, setName] = useState("Lucas")
+
     return (
         <div>
             {condition && <h1>PRINTING</h1> }
+
+            {name === "Lucas" ? (
+                <div>
+                    <p>The name is Lucas</p>
+                </div>
+            ) : (
+                <div>
+                    <p>Name not found</p>
+                </div>
+            )}
+
+            <button onClick={() => setName("Robert")}>Change name!</button>
         </div>
     )
 }
