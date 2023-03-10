9031ff77436ff08329051803fda15996bde14653
Lucas Primo <lucasprimo375@hotmail.com>
2023-03-10 08:54:35 -0300

    component css

--- 6_react_and_css/react_and_css/src/App.js
+++ 6_react_and_css/react_and_css/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1>React with CSS</h1>      
+      <h1>React with CSS</h1>
+
+      <MyComponent></MyComponent>
+
+      <p>This text is from App.js</p>
     </div>
   );
 }
---
+++ 6_react_and_css/react_and_css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>Component CSS</h1>
+        <p>This is the component text</p>
+        <p className="myComponentText">This is another text from the component</p>
+    </div>
+  )
+}
+
+export default MyComponent
