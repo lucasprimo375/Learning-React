6407c8e24fc494ee5800ac7d019959f5385f1c7f
Lucas Primo <lucasprimo375@hotmail.com>
2023-03-10 09:03:26 -0300

    css inline

--- 6_react_and_css/react_and_css/src/App.js
+++ 6_react_and_css/react_and_css/src/App.js
@@ -9,6 +9,8 @@ function App() {
       <MyComponent></MyComponent>
 
       <p>This text is from App.js</p>
+
+      <p style={{color: "pink", padding: "25px", borderTop: "2px solid red"}}>Element stylized inline</p>
     </div>
   );
 }
