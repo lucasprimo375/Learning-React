81c476d4fbe4044fd5fa893f247049ea2700657b
Lucas Primo <lucasprimo375@hotmail.com>
2023-03-10 09:21:11 -0300

    dyncamic inline css

--- 6_react_and_css/react_and_css/src/App.js
+++ 6_react_and_css/react_and_css/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+  const n = 15;
+  const name = "Lucas"
+
   return (
     <div className="App">
       <h1>React with CSS</h1>
@@ -11,6 +14,12 @@ function App() {
       <p>This text is from App.js</p>
 
       <p style={{color: "pink", padding: "25px", borderTop: "2px solid red"}}>Element stylized inline</p>
+
+      <h2 style={n < 10 ? ({color: "purple"}) : ({color: "indigo"})}>Dynamic CSS</h2>
+
+      <h2 style={n > 10 ? ({color: "purple"}) : ({color: "indigo"})}>Another dynamic CSS</h2>
+
+      <h2 style={name === "Lucas" ? ({color: "magenta"}) : null}>One more dynamic CSS</h2>
     </div>
   );
 }
