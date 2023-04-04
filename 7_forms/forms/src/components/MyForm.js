14b04b3e7111c1b82b30e6b1d28517e97e99f9d3
Lucas Primo <lucasprimo375@hotmail.com>
2023-04-04 13:11:50 -0300

    texarea with react

--- 7_forms/forms/src/components/MyForm.js
+++ 7_forms/forms/src/components/MyForm.js
@@ -4,6 +4,7 @@ import {useState} from "react"
 const MyForm = ({user}) => {
   const [name, setName] = useState(user ? user.name : "")
   const [email, setEmail] = useState(user ? user.email : "")
+  const [bio, setBio] = useState("");
 
   const handleName = (e) => {
     setName(e.target.value)
@@ -12,10 +13,11 @@ const MyForm = ({user}) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("sending the form")
-    console.log(name, email);
+    console.log(name, email, bio);
 
     setName("");
     setEmail("");
+    setBio("");
   };
 
   return (
@@ -28,6 +30,10 @@ const MyForm = ({user}) => {
             <label>
                 <span>E-mail:</span>
                 <input type="email" name="email" placeholder="Type your e-mail" onChange={(e) => setEmail(e.target.value)} value={email} />
+            </label>
+            <label>
+              <span>Biography:</span>
+              <textarea name="bio" placeholder="User description" onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
             </label>
             <input type="submit" value="Send" />
         </form>
