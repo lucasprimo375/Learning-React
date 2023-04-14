10edf0aa78ffc8adb0c523d41b209c33fde6ddf4
Lucas Primo <lucasprimo375@hotmail.com>
2023-04-14 16:36:31 -0300

    secret word project first commit

---
+++ 2_project_secret_word/secret_word/src/data/words.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+    car: ["Motor", "Door", "Hood", "Tire", "Antena"],
+    fruit: ["Banana", "Apple", "Pera", "Papaya", "Orange"],
+    body: ["Arm", "Leg", "Brain", "Neck", "Eyes"],
+    computer: ["Mouse", "Keyboard", "Monitor", "Case"],
+    programming: ["Language", "Framework", "JavaScript", "React"],
+    food: ["Rice", "Bean", "Meat", "Milk", "Egg"],
+    movies: ["Avatar", "Aliens", "Terminator", "Titanic", "Alien"]
+};
