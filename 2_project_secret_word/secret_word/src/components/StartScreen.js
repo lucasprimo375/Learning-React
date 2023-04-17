c740607236835de034272c1dc7437756fcd2f70e
Lucas Primo <lucasprimo375@hotmail.com>
2023-04-17 16:24:36 -0300

    creating start screen for the game

---
+++ 2_project_secret_word/secret_word/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = () => {
+    return (
+    <div className="startScreen">
+        <h1>Secret Word</h1>
+        <p>Click on the button below to start a match</p>
+        <button>Start Match</button>
+    </div>
+)
+}
+
+export default StartScreen
